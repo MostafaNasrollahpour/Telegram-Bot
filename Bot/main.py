1b30bd01fc950ce66f4ceef0db2350e45a2310e1
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-25 12:47:30 +0330

    project stopped

--- Bot/main.py
+++ Bot/main.py
@@ -1,1 +1,45 @@
-print('hello world')
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+
+TOKEN = "7567586010:AAEq1IMn-IUNjdENk3xucIcY9R-m8FqY420"
+BOT_USERNAME = "@yousef_joon_bot"  
+
+
+# Command handlers
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلاممم. من یوسفم. بکن سجاد. اسم تو چیه؟")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("میخوای کمکت کنم؟ به تخمم")
+
+
+# Message handlers
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    response = f"You said: {text}"
+    await update.message.reply_text(response)
+
+
+# Error handler
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+
+
+if __name__ == "__main__":
+    print("Starting bot...")
+    app = Application.builder().token(TOKEN).build()
+
+    # Command handlers
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+
+    # Message handler
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Error handler
+    app.add_error_handler(error)
+
+    # Start polling
+    print("Polling...")
+    app.run_polling(poll_interval=3)
